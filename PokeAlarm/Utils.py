51deae6f4d3d6ecd45ddb41d04d4f7401dd93aef
HowDoesExcelWork <HowDoesExcelWork@Outlook.com>
2017-03-16 02:10:35 -0600

    Ditto Past Form Adds flag <previous_id> Also added a get_pokemon_name function to utils.py in order to use the previous_id being sent from HowDoesExcelWork/RocketMap

--- PokeAlarm/Utils.py
+++ PokeAlarm/Utils.py
@@ -91,6 +91,20 @@ def get_pkmn_id(pokemon_name):
     return get_pkmn_id.ids.get(name)
 
 
+def get_pkmn_name(pokemon_id):
+    match = int(pokemon_id)
+    files = glob(get_path('locales/en/pokemon.json'))
+    for file_ in files:
+        with open(file_, 'r') as f:
+            j = json.loads(f.read())
+            for pb in j:
+                # log.warning('ID %s AND pokemon_id %s', pb, pokemon_id)
+                if int(pb) == match:
+                    # log.warning('ID MATCHED WITH GIVEN: NAME IS %s', j[pb])
+                    return j[pb]
+                    break
+
+
 # Returns the id corresponding with the move (use all locales for flexibility)
 def get_move_id(move_name):
     name = move_name.lower()
@@ -236,10 +250,6 @@ def get_gmaps_link(lat, lng):
     latlng = '{},{}'.format(repr(lat), repr(lng))
     return 'http://maps.google.com/maps?q={}'.format(latlng)
 
-#Returns a String link to Apple Maps Pin at the location	
-def get_applemaps_link(lat, lng):
-	latLon = '{},{}'.format(repr(lat), repr(lng))
-	return 'http://maps.apple.com/maps?daddr={}&z=10&t=s&dirflg=w'.format(latLon)
 
 # Returns a static map url with <lat> and <lng> parameters for dynamic test
 def get_static_map_url(settings, api_key=None):  # TODO: optimize formatting
